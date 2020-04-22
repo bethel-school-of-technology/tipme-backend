3254a5f9879e74f5cf521c5dba9c58cce69c2cb4
Jimmy Saintel <41840320+jimmysaintel@users.noreply.github.com>
2020-04-21 21:18:39 -0400

    Revert "thegoose"

--- models/users.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose'); // This allows use of mongoose in this file
-
-var nameSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-   });  // <-- creates models schema for this particular models
-
-module.exports = mongoose.model('users', nameSchema); //<--  this exports the schema witha custom name.耀
